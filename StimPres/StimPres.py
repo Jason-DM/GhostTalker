9842043e9caeb876252afbad1573445ee3f55d52
helbig-17 <helbig.17@osu.edu>
2023-03-23 16:50:33 -0400

    Background Data

--- StimPres/StimPres.py
+++ StimPres/StimPres.py
@@ -230,7 +230,7 @@ def one_block(slides_place):
 
     data = board.get_board_data()
     board.stop_stream()
-    naming_convention = initials + '_' + str(slides_place) +'_5' 
+    naming_convention = initials + '_' + str(slides_place) +'_BC367' 
     # Old Naming Convention:
     # naming_convention = initials + str(test_slides[slides_place][1]) + '_' + slides[slides_place][0]
     DataFilter.write_file(data, naming_convention + '.txt', 'w')
