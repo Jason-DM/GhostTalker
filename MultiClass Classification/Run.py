3bfd8e2cbd79162cd00a81391eb2a0bb029d9b32
Jason Moore <jason42356@gmail.com>
2023-04-03 22:17:07 -0400

    Testing get_filepaths
    .

--- MultiClass Classification/Run.py
+++ MultiClass Classification/Run.py
@@ -1,27 +1,23 @@
-# %%
-# Imports
-# import numpy as np
-# import scipy as sp
-# import pandas as pd
-# from scipy import signal, arange, fft, fromstring, roll
-# from scipy.signal import butter, lfilter, ricker
-# import os
-# import glob
-# import re
-# from sklearn.neighbors import KNeighborsClassifier
-# from sklearn.feature_selection import RFE
-# from sklearn.svm import SVR
 
-# from sklearn.model_selection import cross_val_score
-# from sklearn import metrics
-# from sklearn.cluster import DBSCAN
-# from sklearn.metrics import recall_score, precision_score, f1_score, accuracy_score
-# from scipy.stats import stats
+# Imports
+import numpy as np
+import scipy as sp
+import pandas as pd
+from scipy import signal, arange, fft, fromstring, roll
+from scipy.signal import butter, lfilter, ricker
+import os
+import glob
+import re
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.feature_selection import RFE
+from sklearn.svm import SVR
+
+from sklearn.model_selection import cross_val_score
+from sklearn import metrics
+from sklearn.cluster import DBSCAN
+from sklearn.metrics import recall_score, precision_score, f1_score, accuracy_score
+from scipy.stats import stats
 import supportFunctions as sp
 
-# %%
+file_paths, bg_sample, phoneme_labels = sp.get_filepaths('E:\\Users\\Jason\\Documents\\GitHub\\GhostTalker\\TestData\\DLR_Tests\\3-21-2023')
 
-file_paths, bg_sample, phoneme_labels = sp.get_filepaths('C:\Users\surya\Documents\GitHub\GhostTalker\TestData\DLR_Tests\3-21-2023')
-
-
-# %%
