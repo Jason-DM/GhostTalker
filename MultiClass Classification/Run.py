89c3529a35f6638f67612cd7e7627a0064fa8555
blazin-byte <80867520+blazin-byte@users.noreply.github.com>
2023-04-06 18:05:47 -0400

    Markers and L1 Feature Selection Done

--- MultiClass Classification/Run.py
+++ MultiClass Classification/Run.py
@@ -1,5 +1,7 @@
 # %%
 # Imports
+from sklearn.feature_selection import SelectFromModel
+from sklearn.linear_model import LogisticRegression
 from sklearn.datasets import make_classification
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import numpy as np
@@ -26,6 +28,7 @@ import supportFunctions as sp
 # %%
 # TODO: Preprocessing:
 # Try DB2 Transform
+# Subtract weighted welch of each background from signal
 # Implement HighPass Filter
 # Use markers
 # After those:
@@ -40,6 +43,7 @@ import supportFunctions as sp
 
 # TODO: Model and Analysis:
 # Implement One Vs One Model
+#
 # Print performance stats (which phoneme does best and which does worst)
 
 print('Setting parameters...')
@@ -65,21 +69,31 @@ phoneme_labels = phoneme_labels1+phoneme_labels2
 bg_samples = [bg_sample1 for i in range(
     len(file_paths1))]+[bg_sample2 for i in range(len(file_paths2))]
 
+# TODO: First you'll need to split one file into 5
+# TODO: Then you'll have to work on for-loop to use df instead of file name
+# TODO: Finally you'll also have to make 5 of each label...
+nameFile = file_paths[0]
+df = pd.read_csv(nameFile, sep='\t', header=None)
+
 data_list = []
 labels_vector = []
 for (file_path, phoneme_label, bg_sample) in zip(file_paths, phoneme_labels, bg_samples):
     nameFile = file_path
-    df = pd.read_csv(nameFile, sep='\t')
-    N_SAMPLES = df.shape[0]
-    df.columns = [i for i in range(32)]
-    df = df[df.columns[1:16]]  # Only using the 16 channles as features
+    df = pd.read_csv(nameFile, sep='\t', header=None)
+    # df = df[df.columns[1:16]]  # Only using the 16 channles as features
     feature_vector = []
-    for col in df.columns:
-        subsections = np.array_split(df[col], NUM_WINDOWS)
-        means = [sub.mean() for sub in subsections]
-        feature_vector.extend(means)
-    data_list.append(feature_vector)
-    labels_vector.append(phoneme_label)
+    if nameFile == 'C:\\Users\\surya\\Documents\\GitHub\\GhostTalker\\TestData\\DLR_Tests\\3-21-2023\\DLR_27_2.txt':
+        n = 3
+    else:
+        n = 6
+    for i in range(1, n):
+        df_i = df.iloc[df.loc[df[31] == i].index[0]                       :df.loc[df[31] == i].index[1], :]
+        for col in df_i.columns:
+            subsections = np.array_split(df_i[col], NUM_WINDOWS)
+            means = [sub.mean() for sub in subsections]
+            feature_vector.extend(means)
+        data_list.append(feature_vector)
+        labels_vector.append(phoneme_label)
 
 min_length = min(len(row) for row in data_list)
 # slice all the rows to the length of the shortest one
@@ -111,6 +125,19 @@ svm_object = svm.SVC(probability=False, kernel="rbf",
 svm_object.fit(Xs, y)
 sp.multiclass_performance(Xs, y, logreg)
 sp.multiclass_performance(Xs, y, svm_object)
+
+# %%
+# Feature Selection:
+
+# Set the regularization parameter C = 1
+logistic = LogisticRegression(
+    C=1, penalty='l1', solver='liblinear', random_state=7).fit(Xs, y)
+model = SelectFromModel(logistic, prefit=True)
+
+X_new = model.transform(Xs)
+
+# Dropping Features
+#selected_columns = selected_features.columns[selected_features.var() == 0]
 
 # %%
 # Test Data
